6a5a411e4ec208d83be27881574050f0f6aa282a
segurosgm <segurosgm874@gmail.com>
2025-03-31 10:10:04 -0500

    31-03-24
    Migracion del cod de PHP  a JAVA

---
+++ Java/gmk/src/main/java/com/gmkseguro/backend/gmk/GmkApplication.java
@@ -0,0 +1,13 @@
+package com.gmkseguro.backend.gmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmkApplication.class, args);
+	}
+
+}
---
+++ Java/gmk/src/test/java/com/gmkseguro/backend/gmk/GmkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmkseguro.backend.gmk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
